261bf5f769833e6a28d71bf00c610b8a8aa01bbd
Pasindu Senarathne <pasindubahagya@gmail.com>
2020-03-25 04:17:30 +0530

    2020-03-24 LateN

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -6,16 +6,20 @@ import Modal from "../Contacts";
 
 const Navbar = () => {
   return (
-    <nav class="navbar fixed-top navbar-light bg-light">
-      <a class="navbar-brand" href="/">
-        <img src={logo} className="link-logo" alt="" />
-      </a>
-      <div>
-        <a href="/HealthAdvices" class="btn btn-outline-danger">
-          <b>කොරෝනා ගැන දැනගන්න, ප්‍රවේශම් වන්න!</b>
-        </a>
-      </div>
-    </nav>
+    <div>
+      <nav class="navbar fixed-top navbar-light bg-light">
+        <div style={{ display: "table", marginRight: "auto !important" }}>
+          <a class="navbar-brand" href="/">
+            <img src={logo} className="link-logo" alt="" />
+          </a>
+        </div>
+        <div>
+          <a href="/HealthAdvices" class="btn btn-outline-danger">
+            <b>කොරෝනා ගැන දැනගන්න, ප්‍රවේශම් වන්න!</b>
+          </a>
+        </div>
+      </nav>
+    </div>
   );
 };
 
